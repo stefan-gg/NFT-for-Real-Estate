766dd8bba7a6ac4f590075dbc1ca75bea2fb4dd3
stefan-gg <stefan.gogic01@gmail.com>
2024-05-27 00:01:16 +0200

    Netlify fix #7

--- nft-for-real-estate/src/App.js
+++ nft-for-real-estate/src/App.js
@@ -37,45 +37,18 @@ function App() {
     }
   };
 
-  // const loadAccounts = async () => {
-  //   const accounts = await provider.send('eth_accounts', []);
-  //   updateAccounts(accounts);
-  // };
+  const loadAccounts = async () => {
+    const accounts = await provider.send('eth_accounts', []);
+    updateAccounts(accounts);
+  };
 
-  // const updateAccounts = async accounts => {
-  //   if (provider) {
-  //     if (accounts && accounts.length > 0) {
-  //       setUser({
-  //         signer: await provider.getSigner(),
-  //         balance: await provider.getBalance(accounts[0]),
-  //       });
-
-  //       setStateChanger(new StateChanger(await provider.getSigner()));
-  //     } else {
-  //       setUser({ signer: null, balance: 0 });
-  //     }
-  //   }
-  // };
-
-  // const handleConnectWallet = async () => {
-  //   setIsMinting(true);
-  //   setIsConnecting(true);
-
-  //   try {
-  //     const accounts = await provider.send('eth_requestAccounts', []);
-  //     toast({ title: 'Wallet connected', status: 'success' });
-  //     updateAccounts(accounts);
-  //   } catch (error) {
-  //     toast({
-  //       title: 'Wallet connection failed',
-  //       status: 'error',
-  //       description: error.code,
-  //     });
-  //   }
-
-  //   setIsConnecting(false);
-  //   setIsMinting(false);
-  // };
+  const updateAccounts = useCallback((accounts) => {
+    if (provider) {
+      handleUpdateAccounts(accounts);
+    } else {
+      setUser({ signer: null, balance: 0 });
+    }
+  }, [provider]);
 
   const withdrawNFT = async (tokenId) => {
     setWithdraw(true);
@@ -526,45 +499,7 @@ function App() {
 
   useEffect(() => {
     if (provider) {
-      const loadAccounts = async () => {
-        const accounts = await provider.send('eth_accounts', []);
-        updateAccounts(accounts);
-      };
-
-      const updateAccounts = async accounts => {
-        if (provider) {
-          if (accounts && accounts.length > 0) {
-            setUser({
-              signer: await provider.getSigner(),
-              balance: await provider.getBalance(accounts[0]),
-            });
-    
-            setStateChanger(new StateChanger(await provider.getSigner()));
-          } else {
-            setUser({ signer: null, balance: 0 });
-          }
-        }
-      };
-
-      const handleConnectWallet = async () => {
-        setIsMinting(true);
-        setIsConnecting(true);
-    
-        try {
-          const accounts = await provider.send('eth_requestAccounts', []);
-          toast({ title: 'Wallet connected', status: 'success' });
-          updateAccounts(accounts);
-        } catch (error) {
-          toast({
-            title: 'Wallet connection failed',
-            status: 'error',
-            description: error.code,
-          });
-        }
-    
-        setIsConnecting(false);
-        setIsMinting(false);
-      };
+      loadAccounts();
 
       const _collectionService = new CollectionService(provider);
       _collectionService.getAllNFTs().then(_list => setList(_list));
